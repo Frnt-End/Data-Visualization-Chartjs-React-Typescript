4c3762977f98e9cb4441b6edcb8e3fa28f686025
Nirit Nagar <nirit.nn@gmail.com>
2023-08-13 22:47:30 +0200

    Adding dist

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Data-Visualization-Chartjs-React-Typescript/",
+  base: "/",
   plugins: [react()],
   optimizeDeps: {
     include: ["@emotion/react", "@emotion/styled"],
